dc14cdfe585e4fd5d747583b9572bf7bd399e555
Ana Beatriz Pereira de Almeida <20221170150023@ifrn.local>
2024-01-03 12:01:14 -0300

    mudança

--- TRABALHO/Assets/Arkanoid/Scripts/GameManager.cs
+++ TRABALHO/Assets/Arkanoid/Scripts/GameManager.cs
@@ -1,74 +1,91 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPPro;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-public static GameManager instance;
-public int vidas = 2;
-public int tijolosRestantes; 
+    public static GameManager instance;
+    public int vidas = 2;
+    public int tijolosRestantes;
 
-public GameObject playerPrefab;
-public GameObject ballPrefab;
+    public GameObject playerPrefab;
+    public GameObject ballPrefab;
 
-public Transform PlayerSpawnPoint;
-public Transform BallSpawnPoint;
+    public Transform PlayerSpawnPoint;
+    public Transform BallSpawnPoint;
 
-public PlayerB playerAtual;
-public BallB ballAtual;
+    public PlayerB playerAtual;
+    public BallB ballAtual;
 
-public TextMeshProUGUI Contador;
-public TextMeshProUGUI MsgFinal;
-public bool segurando;
-private Vector3 offset;
+    public TextMeshProUGUI contador;
+    public TextMeshProUGUI MsgFinal;
+    public bool segurando;
+    private Vector3 offset;
 
 
-private void Awake()
-{
-    instance = this;
-}
-    // Start is called before the first frame update
-
-      void Start()
+    private void Awake()
     {
-        
+        instance = this;
     }
-   
- void Start()
-{
-   SpawnarNovoJogador();
-   AtualizarContador();
-  
-}
-public void AtualizarContador()
-{
-    contador.text = $"Vidas: {vidas}";
-}
-  
 
-public void SpawnarNovoJogador()
-{
-    GameObject playerObj =  Instantiate(playerPrefab, PlayerSpawnPointPlayerB.position, Quaternion.identity);
-    GameObject ballObj = Instantiate(ballPrefab, SpawnPointBallB.position, Quaternion.identity);
+    void Start()
+    {
+        SpawnarNovoJogador();
+        AtualizarContador();
+        tijolosRestantes = FindGameObjectsWithTag("Tijolo").Leght;
+
+    }
+    public void AtualizarContador()
+    {
+        contador.text = $"Vidas: {vidas}";
+    }
+
+
+    public void SpawnarNovoJogador()
+    {
+        GameObject playerObj = Instantiate(playerPrefab, PlayerSpawnPoint.position, Quaternion.identity);
+        GameObject ballObj = Instantiate(ballPrefab, BallSpawnPoint.position, Quaternion.identity);
 
         playerAtual = playerObj.GetComponent<PlayerB>();
-        ballAtual =  ballObj.GetComponent<BallB>();
+        ballAtual = ballObj.GetComponent<BallB>();
         segurando = true;
-        offset - playerAtual.transform.position - ballAtual.transform.position;
-        
+        offset = playerAtual.transform.position - ballAtual.transform.position;
+
     }
 
-void update()
-{
-    if (segurando)
+    public void subtrairTijolo() 
     {
+        tijolosRestantes--;
+        if (tijolosRestantes <= 0)
+        {
+            Vitoria();
+        }
+    }
 
-         ballAtual = transform.position = playerAtual.transform.position- offset;
+    public void subtrairTVida()
+    {
+    }
+    public void Vitoria()
+    {
+    }
+    public void GameOver()
+    {
+    }
 
-         if(input.GetkeyDown(keyCode.Space)){
-            ballAtual.DispararBolinha(playerAtual.inputX);
-            segurando =  false;
-         }
+
+    void update()
+    {
+        if (segurando)
+        {
+
+            ballAtual.transform.position = playerAtual.transform.position - offset;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ballAtual.DispararBolinha(playerAtual.inputX);
+                segurando = false;
+            }
+        }
     }
 }
